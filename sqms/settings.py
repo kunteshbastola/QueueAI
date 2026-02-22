f591ede0a18d7efe1d95fcb7baaafa89bba12ee0
kunteshbastola <kunteshbastola5@gmail.com>
2026-02-22 14:36:58 +0545

    commit

--- sqms/settings.py
+++ sqms/settings.py
@@ -54,14 +54,19 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',   # MUST be first
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://your-frontend-domain.com",
 ]
 
 ROOT_URLCONF = 'sqms.urls'
